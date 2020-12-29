12e26eed48cb57dac59236d584e75ea90246a5f2
Aaron Sarazan <aaron@sarazan.net>
2020-12-29 11:12:39 -0800

    pass through req in example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,18 +1,15 @@
 console.log(`Spinning up...`);
 
-function result(handler: string) {
-  console.log(`Execute ${handler}`);
+export const handler = async (req: any, context: any) => {
+  console.log("Request", req?.body ?? req);
   return {
     statusCode: 200,
     headers: {
       "x-custom-header" : "my custom header value"
     },
     body: JSON.stringify({
-      handler
+      handler: "ok!",
+      req: req?.body ?? req
     })
   };
-}
-
-export const handler = async (req: any) => {
-  return result("handler ok!");
 }
