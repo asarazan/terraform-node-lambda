b99c4e6d03bbd239c27f00e8425d82fc921e469b
Aaron Sarazan <aaron@sarazan.net>
2020-12-29 11:05:36 -0800

    update index files

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,7 +1,5 @@
 console.log(`Spinning up...`);
 
-const token = "foobar2";
-
 function result(handler: string) {
   console.log(`Execute ${handler}`);
   return {
@@ -10,16 +8,11 @@ function result(handler: string) {
       "x-custom-header" : "my custom header value"
     },
     body: JSON.stringify({
-      handler,
-      token
+      handler
     })
   };
 }
 
-export const handler1 = async (req: any) => {
-  return result("handler 1");
-}
-
-export const handler2 = async (req: any) => {
-  return result("handler 2");
+export const handler = async (req: any) => {
+  return result("handler ok!");
 }
